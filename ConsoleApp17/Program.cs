b7be7769bcd54409deedf400068300f89dfcfde5
huseynManaf <huseynim@code.edu.az>
2022-12-22 04:11:08 -0800

    init

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int saniye = 273908;
+            int gun = saniye / (24 * 60 * 60);
+            int saat = (saniye - gun * 24 * 60 * 60) / (60 * 60);
+            int deq = (saniye - gun * 24 * 60 * 60 - saat * 60 * 60) / 60;
+            int san = saniye % 60;
+            Console.WriteLine($"{gun} gun {saat} saat {deq}deqiqe {san} saniye");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("master");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
